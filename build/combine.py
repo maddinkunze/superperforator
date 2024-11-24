cb827925db121b2f806104e01cf232bfa12a0111
Martin Kunze <martin@martinkunze.com>
2024-11-24 18:43:23 +0100

    added icon to source code; removed unneccessary /> from <link> tags; fixed bug where jsPDF could not be included in build due to encoding errors; updated build process to reflect those changes

--- build/combine.py
+++ build/combine.py
@@ -2,39 +2,42 @@ import os
 import re
 import base64
 
-SRC_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.pardir, "src"))
+BUILD_DIR = os.path.realpath(os.path.dirname(__file__))
+SRC_DIR = os.path.realpath(os.path.join(BUILD_DIR, os.path.pardir, "src"))
 
-re_style_in_html = re.compile(r"""<link rel="stylesheet" href="([\w\-\/\.]+)" \/>""")
-re_script_in_html = re.compile(r"""<script src="([\w\-\/\.]+)"></script>""")
+re_icon_in_html = re.compile(rb"""<link rel="icon" href="([\w\-\/\.]+)">""")
+re_style_in_html = re.compile(rb"""<link rel="stylesheet" href="([\w\-\/\.]+)">""")
+re_script_in_html = re.compile(rb"""<script src="([\w\-\/\.]+)"></script>""")
+mime_types = {b"svg": b"image/svg+xml"}
 def resolve_dependencies_html(path, name, data):
-  data = re.sub(re_style_in_html, lambda m: "<style>" + resolve_dependencies(os.path.join(path, m[1])) + "</style>", data)
-  data = re.sub(re_script_in_html, lambda m: "<script>" + resolve_dependencies(os.path.join(path, m[1])) + "</script>", data)
+  data = re.sub(re_icon_in_html, lambda m: b"<link rel=\"icon\" href=\"data:"+mime_types[m[1].split(b".")[-1]]+b";base64,"+base64.b64encode(resolve_dependencies(os.path.join(path, m[1].decode())))+b"\">", data)
+  data = re.sub(re_style_in_html, lambda m: b"<style>" + resolve_dependencies(os.path.join(path, m[1].decode())) + b"</style>", data)
+  data = re.sub(re_script_in_html, lambda m: b"<script>" + resolve_dependencies(os.path.join(path, m[1].decode())) + b"</script>", data)
   return data
 
-re_url_in_css = re.compile(r"""(?<=[:\s,])url\(['"]([\w\-\.\/]+)['"]\)\s+format\(['"](\w+)['"]\)""")
-url_types = {"woff2": "font/woff2", "woff": "application/font-woff"}
+re_url_in_css = re.compile(rb"""(?<=[:\s,])url\(['"]([\w\-\.\/]+)['"]\)\s+format\(['"](\w+)['"]\)""")
+url_types = {b"woff2": b"font/woff2", b"woff": b"application/font-woff"}
 def resolve_dependencies_css(path, name, data):
-  data = re.sub(re_url_in_css, lambda m: f"url('data:{url_types[m[2]]};base64,{base64.b64encode(resolve_dependencies(os.path.join(path, m[1]))).decode()}') format('{m[2]}')", data)
+  data = re.sub(re_url_in_css, lambda m: b"url('data:"+url_types[m[2]]+b";base64,"+base64.b64encode(resolve_dependencies(os.path.join(path, m[1].decode())))+b"') format('"+m[2]+b"')", data)
   return data
 
 def resolve_dependencies(filepath):
   path, name = os.path.split(filepath)
   ext = os.path.splitext(name)[1][1:]
-  _mode = "rb" if ext in ["woff", "woff2", "png", "jpg"] else "r"
-  data = open(filepath, _mode).read()
+  data = open(filepath, "rb").read()
   if ext in ["html"]:
     return resolve_dependencies_html(path, name, data)
   elif ext in ["css"]:
     return resolve_dependencies_css(path, name, data)
-  elif ext in ["js", "woff", "woff2", "png", "jpg"]:
+  elif ext in ["js", "woff", "woff2", "png", "jpg", "svg"]:
     return data
   else:
     raise ValueError(f"cannot resolve dependencies for file {name} ({path}) - unregistered extension {ext}")
 
 def main():
   data = resolve_dependencies(os.path.join(SRC_DIR, "index.html"))
-  version = re.search(r"""const\s+APP_VERSION\s*=\s*['"](\d+\.\d+\.\d+)['"]\s*;""", data)[1] # find 'const APP_VERSION = "x.x.x";' line
-  file = open(f"superperforator-v{version}.html", "w")
+  version = re.search(rb"""const\s+APP_VERSION\s*=\s*['"](\d+\.\d+\.\d+)['"]\s*;""", data)[1].decode() # find 'const APP_VERSION = "x.x.x";' line
+  file = open(os.path.join(BUILD_DIR, f"superperforator-v{version}.html"), "wb")
   file.write(data)
   file.close()
 
